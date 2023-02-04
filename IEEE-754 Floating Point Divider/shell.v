1a60b67f87a293d7a784fb220fb4e519a5a92a39
Rachit Singh <rach1t.s1n9h@gmail.com>
2023-02-04 18:10:12 +0530

    Update shell.v

--- IEEE-754 Floating Point Divider/shell.v
+++ IEEE-754 Floating Point Divider/shell.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: IITG
+// Engineer: Rachit Singh
 // 
 // Create Date: 01.02.2023 11:15:42
 // Design Name: 
